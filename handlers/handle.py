4dfa9cc3bc43c6f499d0e17280e1cd400f7e6a29
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-02-27 07:14:56 +0000

    Update handle.py

--- handlers/handle.py
+++ handlers/handle.py
@@ -23,20 +23,17 @@ def message_handl(message:Message,bot:TeleBot):
     try:
         if data['type']=="LONG":
             message1=f"✨{pair}/USDT\n\n🎗 Trade Type = {data['type']} 🟢\n\n💫 Leverage :- {leverage[2]}\n\n⚡️ Entry = [ {entry[0]} TO {entry[1] }]\n\n❌ StopLoss :- {data['stoploss']}\n\n❎ Take profit = [ {data['targets'][0]}, {data['targets'][1]} , {data['targets'][2]} , {data['targets'][3]} , {data['targets'][4]} , {data['targets'][5]} , {data['targets'][6]} ]"
-
-            print(message1)
         else:
             message1=f"✨{pair}/USDT\n\n🎗 Trade Type = {data['type']} 🔴\n\n💫 Leverage :- {leverage[2]}\n\n⚡️ Entry=[ {entry[0]} TO {entry[1] }]\n\n❌ StopLoss= {data['stoploss']}\n\n❎ Take profit=[ {data['targets'][0]}, {data['targets'][1]} {data['targets'][2]} {data['targets'][3]} {data['targets'][4]} {data['targets'][5]} {data['targets'][6]} ]"
     except Exception as e:
         print(e)
     try:
         message2=f"📍 {data['pair']}\n\n🏹 Signal Type:- {data['type']}\n\n💫Leverage: {data['leverage']}\n\n👉 Entry Targets:- {data['entry']}\n\n🎯 Profit Targets:\n1) {data['targets'][0]}\n2) {data['targets'][1]}\n3) {data['targets'][2]}\n4) {data['targets'][3]}\n5) {data['targets'][4]}\n6) {data['targets'][5]}\n7) {data['targets'][6]}\n\n🛑 Stop Target: {data['stoploss']} "
-        print(message2)
     except Exception as e:
         print(e)
     try:
         message3=f"⚡️💫 {data['pair']} 💫⚡️\n\n[{data['type']}]:{data['entry']}\n\n✨🎯 TARGETS ✨🎯\n\n1.Goal👉 {data['targets'][0]}\n2.Goal👉 {data['targets'][1]}\n3.Goal👉 {data['targets'][2]}\n4.Goal👉 {data['targets'][3]}\n5.Goal👉 {data['targets'][4]}\n6.Goal👉 {data['targets'][5]}\n7.Goal👉 {data['targets'][6]}\n\nSL🛑:- {data['stoploss']}\n\n🎗 LEVERAGE:- {data['leverage']}"
-        print(message3)
+        
     except Exception as e:
         print(e)
     messages=[message1,message2,message3]
